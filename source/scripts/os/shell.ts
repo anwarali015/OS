6301e5d8ed3e7f05f08a70a2724b8855eccb5f95
Mohammed Ali <anwarali015@gmail.com>
2014-09-21 19:00:25 -0400

    Load Command all Working

--- source/scripts/os/shell.ts
+++ source/scripts/os/shell.ts
@@ -294,7 +294,7 @@ module TSOS {
 
                 var temp = x.charCodeAt(i);
 
-                if((temp >=65 && temp<=70) || (temp >=48 && temp<=57) || (temp >=97 && temp <=102)){
+                if((temp == 32) || (temp >=65 && temp<=70) || (temp >=48 && temp<=57) || (temp >=97 && temp <=102)){
                     continue;
                 }else{
                     _StdOut.putText("FOUND INVALID HEX CHARACTER!");
