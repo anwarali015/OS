17a1c90b30a7135c614919e1408a4ff4287b5255
Mohammed Ali <anwarali015@gmail.com>
2014-09-21 19:01:00 -0400

    Load Command all Working

--- dist/scripts/os/shell.js
+++ dist/scripts/os/shell.js
@@ -264,7 +264,7 @@ var TSOS;
             for (var i = 0; i < x.length; i++) {
                 var temp = x.charCodeAt(i);
 
-                if ((temp >= 65 && temp <= 70) || (temp >= 48 && temp <= 57) || (temp >= 97 && temp <= 102)) {
+                if ((temp == 32) || (temp >= 65 && temp <= 70) || (temp >= 48 && temp <= 57) || (temp >= 97 && temp <= 102)) {
                     continue;
                 } else {
                     _StdOut.putText("FOUND INVALID HEX CHARACTER!");
